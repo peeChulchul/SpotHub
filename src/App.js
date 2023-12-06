337453a6afbdbd64e295406fe3fde9ea3276eab4
ahneun <146177452+ahneun@users.noreply.github.com>
2023-12-06 11:06:30 +0900

    1차 저장

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Login from 'component/Login';
 import Router from 'shared/Router';
 
 function App() {
-  return <Router />;
+  return (
+    <>
+  <Router />;
+  <Login />;
+  </>
+  )
 }
 
 export default App;
