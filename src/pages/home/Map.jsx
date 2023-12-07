4fd7752d1c14b58672dddfda3390b27757707fbd
Dello96 <enadios03@gmail.com>
2023-12-07 17:06:07 +0900

    리액트 코드로 리팩토링, 헌옷수거함 아이콘수정, 현재위치접근 허용 추가

--- src/pages/home/Map.jsx
+++ src/pages/home/Map.jsx
@@ -1,168 +1,241 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useEffect } from 'react';
 import Header from './Header';
-import clothes from '../../assets/clothes.jpeg';
+import clothes from '../../assets/clothes.png';
 import toilet from '../../assets/toilet.png';
 import marker from '../../assets/marker.png';
 import trash from '../../assets/trash.png';
+import { useNavigate } from 'react-router-dom';
+import { useKakaoLoader, Map as KakaoMap, MapMarker } from 'react-kakao-maps-sdk';
 
 function Map() {
-  const new_script = (src) => {
-    return new Promise((resolve, reject) => {
-      const script = document.createElement('script');
-      script.src = src;
-      script.addEventListener('load', () => {
-        resolve();
-      });
-      script.addEventListener('error', (e) => {
-        reject(e);
-      });
-      document.head.appendChild(script);
-    });
-  };
-  const markers = [];
+  const [loading, error] = useKakaoLoader({
+    appkey: process.env.REACT_APP_KAKAO_MAP_API_KEY // 발급 받은 APPKEY
+    // ...options // 추가 옵션
+  });
+  const navigate = useNavigate();
+
+  const markers = [
+    {
+      position: { lat: 37.478400413698, lng: 127.13538446564 },
+      locationName: '슈퍼쓰레기통',
+      locationid: '임시 아이디',
+      img: trash
+    },
+    {
+      position: { lat: 37.478000413698, lng: 127.13738648584 },
+      locationName: '양철쓰레기통',
+      locationid: '임시 아이디',
+      img: trash
+    },
+    {
+      position: { lat: 37.477800413698, lng: 127.13838849594 },
+      locationName: '구리쓰레기통',
+      locationid: '임시 아이디',
+      img: trash
+    },
+
+    {
+      position: { lat: 37.477619964555, lng: 127.13405884939 },
+      locationName: '슈퍼의류수거함',
+      locationid: '임시 아이디',
+      img: clothes
+    },
+    {
+      position: { lat: 37.477200413698, lng: 127.13438245614 },
+      locationName: '구리양철의류수거함쓰레기통',
+      locationid: '임시 아이디',
+      img: clothes
+    },
+
+    {
+      position: { lat: 37.477000413698, lng: 127.13388245624 },
+      locationName: '슈퍼화장실',
+      locationid: '임시 아이디',
+      img: toilet
+    },
+    {
+      position: { lat: 37.476800413698, lng: 127.13458245634 },
+      locationName: '양철화장실',
+      locationid: '임시 아이디',
+      img: toilet
+    }
+  ];
+
+  // const navigate = useNavigate();
+  // const new_script = (src) => {
+  //   return new Promise((resolve, reject) => {
+  //     const script = document.createElement('script');
+  //     script.src = src;
+  //     script.addEventListener('load', () => {
+  //       resolve();
+  //     });
+  //     script.addEventListener('error', (e) => {
+  //       reject(e);
+  //     });
+  //     document.head.appendChild(script);
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   const my_script = new_script(
+  //     `https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${process.env.REACT_APP_KAKAO_MAP_API_KEY}`
+  //   );
+  //   my_script.then(() => {
+  //     console.log('script loaded!!!');
+  //     const kakao = window['kakao'];
+  //     kakao.maps.load(() => {
+  //       const mapContainer = document.getElementById('map');
+  //       const options = {
+  //         center: new kakao.maps.LatLng(37.477200413358, 127.13438245554), //좌표설정
+  //         level: 3
+  //       };
+  //       const map = new kakao.maps.Map(mapContainer, options); //맵생성
+
+  //       function createMarkerImage(src, size, options) {
+  //         let markerImage = new kakao.maps.MarkerImage(src, size, options);
+  //         return markerImage;
+  //       }
+  //       function createMarker(position, image) {
+  //         const marker = new kakao.maps.Marker({
+  //           position: position,
+  //           image: image
+  //         });
+
+  //         return marker;
+  //       }
+  //       kakao.maps.event.addListener(map, 'click', function (event) {
+  //         console.log(event.latLng);
+  //       });
+
+  //       function createMarkers(array, src, size, point) {
+  //         const imageSize = new kakao.maps.Size(size[0], size[1]);
+  //         const imageOptions = {
+  //           spriteOrigin: new kakao.maps.Point(point[0], point[1])
+  //         };
+  //         const markerImage = createMarkerImage(src, imageSize, imageOptions);
+  //         array.forEach((markerData, index) => {
+  //           const marker = createMarker(markerData.position, markerImage);
+  //           kakao.maps.event.addListener(marker, 'click', function (event) {
+  //             alert(`마커 클릭: ${markerData.location}`);
+  //             // navigate(`/:${markerData.location}`);
+  //           });
+  //           markers.push(marker);
+  //         });
+  //       }
+
+  //       createMarkers(trashPositions, trash, [30, 30], [0, 0]);
+  //       createMarkers(clothesPositions, clothes, [40, 40], [0, 0]);
+  //       createMarkers(toiletPositions, toilet, [40, 40], [0, 0]);
+
+  //       function setMarkers(map) {
+  //         for (let i = 0; i < markers.length; i++) {
+  //           markers[i].setMap(map);
+  //         }
+  //       }
+
+  //       setMarkers(map);
+  //     });
+  //   });
+
+  //   return () => {
+  //     const kakao = window['kakao'];
+
+  //     // 새로추가된 이벤트를 컴포넌트 언마운트시 제거할수있도록 removeListener를 통해 제거해야함
+  //     markers.forEach((marker) => {
+  //       kakao.maps.event.removeListener(marker, 'click');
+  //       marker.setMap(null);
+  //     });
+  //   };
+  // }, []);
+
+  const [userLocation, setUserLocation] = useState({
+    center: {
+      lat: 33.450701,
+      lng: 126.570667
+    },
+    errMsg: null,
+    isLoading: true
+  });
 
   useEffect(() => {
-    const my_script = new_script(
-      `https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${process.env.REACT_APP_KAKAO_MAP_API_KEY}`
-    );
-    my_script.then(() => {
-      console.log('script loaded!!!');
-      const kakao = window['kakao'];
-      kakao.maps.load(() => {
-        const mapContainer = document.getElementById('map');
-        const options = {
-          center: new kakao.maps.LatLng(37.477200413358, 127.13438245554), //좌표설정
-          level: 3
-        };
-        const map = new kakao.maps.Map(mapContainer, options); //맵생성
-
-        const trashPositions = [
-          new kakao.maps.LatLng(37.478400413698, 127.13538446564),
-          new kakao.maps.LatLng(37.478200413698, 127.13638247574),
-          new kakao.maps.LatLng(37.478000413698, 127.13738648584),
-          new kakao.maps.LatLng(37.477800413698, 127.13838849594)
-        ];
-
-        const clothesPositions = [
-          new kakao.maps.LatLng(37.477619964555, 127.13405884939),
-          new kakao.maps.LatLng(37.477200413698, 127.13438245614)
-        ];
-
-        const toiletPositions = [
-          new kakao.maps.LatLng(37.477000413698, 127.13388245624),
-          new kakao.maps.LatLng(37.476800413698, 127.13458245634)
-        ];
-
-        const trashMakers = [];
-        const clothesMakers = [];
-        const toiletMakers = [];
-
-        createTrashMarkers();
-        createClothesMarkers();
-        createToiletMarkers();
-
-        function createMarkerImage(src, size, options) {
-          let markerImage = new kakao.maps.MarkerImage(src, size, options);
-          return markerImage;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserLocation((prev) => ({
+            ...prev,
+            center: {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            },
+            isLoading: false
+          }));
+        },
+        (err) => {
+          setUserLocation((prev) => ({
+            ...prev,
+            errMsg: err.message,
+            isLoading: false
+          }));
         }
-        function createMarker(position, image) {
-          const marker = new kakao.maps.Marker({
-            position: position,
-            image: image
-          });
-
-          return marker;
-        }
-
-        function createTrashMarkers() {
-          for (let i = 0; i < trashPositions.length; i++) {
-            const imageSize = new kakao.maps.Size(30, 30);
-            const imageOptions = {
-              spriteOrigin: new kakao.maps.Point(0, 0)
-              //   spriteSize: new kakao.maps.Size(36, 98)
-            };
-
-            // 마커이미지와 마커를 생성합니다
-            const markerImage = createMarkerImage(trash, imageSize, imageOptions);
-            const marker = createMarker(trashPositions[i], markerImage);
-
-            // 생성된 마커를 쓰레기통 마커 배열에 추가합니다
-            markers.push(marker);
-          }
-        }
-        function setTrashMarkers(map) {
-          for (let i = 0; i < trashMakers.length; i++) {
-            trashMakers[i].setMap(map);
-          }
-        }
-
-        function createClothesMarkers() {
-          for (let i = 0; i < clothesPositions.length; i++) {
-            const imageSize = new kakao.maps.Size(50, 50);
-            const imageOptions = {
-              spriteOrigin: new kakao.maps.Point(5, 5)
-              //   spriteSize: new kakao.maps.Size(36, 98)
-            };
-
-            // 마커이미지와 마커를 생성합니다
-            const markerImage = createMarkerImage(clothes, imageSize, imageOptions);
-            const marker = createMarker(clothesPositions[i], markerImage);
-            console.log(clothesPositions);
-
-            // 생성된 마커를 쓰레기통 마커 배열에 추가합니다
-            markers.push(marker);
-          }
-        }
-        function setClothesMarkers(map) {
-          for (let i = 0; i < clothesMakers.length; i++) {
-            clothesMakers[i].setMap(map);
-          }
-        }
-        function createToiletMarkers() {
-          for (let i = 0; i < toiletPositions.length; i++) {
-            const imageSize = new kakao.maps.Size(40, 40);
-            const imageOptions = {
-              spriteOrigin: new kakao.maps.Point(0, 0)
-              //   spriteSize: new kakao.maps.Size('center', 'center')
-            };
-
-            // 마커이미지와 마커를 생성합니다
-            const markerImage = createMarkerImage(toilet, imageSize, imageOptions);
-            const marker = createMarker(toiletPositions[i], markerImage);
-
-            // 생성된 마커를 쓰레기통 마커 배열에 추가합니다
-            markers.push(marker);
-          }
-        }
-        function setMarkers(map) {
-          for (let i = 0; i < markers.length; i++) {
-            markers[i].setMap(map);
-          }
-        }
-
-        function setToiletMarkers(map) {
-          for (let i = 0; i < toiletMakers.length; i++) {
-            toiletMakers[i].setMap(map);
-          }
-        }
-        console.log(markers);
-        setMarkers(map);
-        // setTrashMarkers(map);
-        // setClothesMarkers(map);
-        // setToiletMarkers(map);
-      });
-    });
+      );
+    } else {
+      setUserLocation((prev) => ({
+        ...prev,
+        errMsg: '위치를 읽을 수 없습니다.',
+        isLoading: false
+      }));
+    }
   }, []);
 
-  return <WrappingMap id="map" className="map" />;
+  function onClickMap(_t, mouseEvent) {
+    console.log(mouseEvent.latLng);
+  }
+
+  return (
+    <WrappingMap>
+      <KakaoMap // 지도를 표시할 Container
+        onClick={onClickMap}
+        id="map"
+        center={{
+          // 지도의 중심좌표
+          lat: 37.478400413698,
+          lng: 127.13538446564
+        }}
+        style={{
+          // 지도의 크기
+          width: '100%',
+          height: '100%'
+        }}
+        level={3} // 지도의 확대 레벨
+      >
+        {markers.map(({ position, img, locationName, locationid }, index) => (
+          <MapMarker
+            key={index}
+            position={{
+              lat: position.lat,
+              lng: position.lng
+            }}
+            image={{
+              src: img,
+              size: { width: 30, height: 30 },
+              options: {
+                spriteSize: { width: 30, height: 30 },
+                spriteOrigin: { x: 0, y: 0 }
+              }
+            }}
+          ></MapMarker>
+        ))}
+      </KakaoMap>
+    </WrappingMap>
+  );
 }
 
 const WrappingMap = styled.div`
-  display: flex;
   width: 100%;
   height: 100vh;
+  background-color: black;
 `;
 
 export default Map;
